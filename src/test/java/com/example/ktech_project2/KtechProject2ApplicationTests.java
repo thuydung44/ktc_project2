7d706a41d15fa305f52708b0d58eaf8d3d887da3
Dung Thùy <dungkiu476@gmail.com>
2024-08-12 09:44:28 +0900

    start

---
+++ src/test/java/com/example/ktech_project2/KtechProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktech_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtechProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
